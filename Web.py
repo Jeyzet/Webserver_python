5d6e9ce78d0f95c0c08c6cb04777c8319e0f5e6d
J0hnz_0350 <68404907+Jeyzet@users.noreply.github.com>
2022-06-12 10:37:18 -0400

    Create Web.py

---
+++ Web.py
@@ -0,0 +1,29 @@
+# Python 3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "Escribe aqui la dirección IP de tu servidor"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Curso Udemy - Network Security</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>Felicitaciones!, el servidor Web esta funcionando.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Accede al servidor a traves de la URL ---> http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
